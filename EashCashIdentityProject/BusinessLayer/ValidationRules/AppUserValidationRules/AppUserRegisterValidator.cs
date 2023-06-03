4c730482866692b16726c66867ae87c153484309
faarukbulut <faarukbulut@gmail.com>
2023-06-03 07:46:06 +0300

    AppUserRegister Fluent Validation

---
+++ EashCashIdentityProject/BusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -0,0 +1,24 @@
+﻿using DtoLayer.Dtos.AppUserDtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules.AppUserValidationRules
+{
+    public class AppUserRegisterValidator : AbstractValidator<AppUserRegisterDto>
+    {
+        public AppUserRegisterValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Boş Bırakılamaz");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Boş Bırakılamaz");
+            RuleFor(x => x.Username).NotEmpty().WithMessage("Boş Bırakılamaz");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Boş Bırakılamaz");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Boş Bırakılamaz");
+            RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("Boş Bırakılamaz");
+            RuleFor(x => x.ConfirmPassword).Equal(y => y.Password).WithMessage("Şifreler eşleşmiyor");
+        }
+    }
+}
