67865ca61278f0295fffee7f3323485070ff5635
faarukbulut <faarukbulut@gmail.com>
2023-06-03 07:16:01 +0300

    4- Referans işlemleri ve Identity eklemesi

---
+++ EashCashIdentityProject/DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : IdentityDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=FARUK\\SQLEXPRESS;initial catalog=EasyCashDb;integrated Security=true");
+        }
+
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+
+        public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; }
+
+
+    }
+}
