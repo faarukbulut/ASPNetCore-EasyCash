18808480d12c06d8fecddaf9ffd9c82733219c48
faarukbulut <faarukbulut@gmail.com>
2023-06-03 20:38:18 +0300

    21 - Login Yapan Kullanıcı Bilgilerini Getirme

--- EashCashIdentityProject/PresentationLayer/Controllers/MyProfileController.cs
+++ EashCashIdentityProject/PresentationLayer/Controllers/MyProfileController.cs
@@ -1,12 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DtoLayer.Dtos.AppUserDtos;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace PresentationLayer.Controllers
 {
 	public class MyProfileController : Controller
 	{
-		public IActionResult Index()
+		private readonly UserManager<AppUser> _userManager;
+
+        public MyProfileController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+		[HttpGet]
+        public async Task<IActionResult> Index()
 		{
-			return View();
+			var values = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            AppUserEditDto appUserEditDto = new AppUserEditDto();
+
+            appUserEditDto.Name = values.Name;
+            appUserEditDto.Surname = values.Surname;
+            appUserEditDto.PhoneNumber = values.PhoneNumber;
+            appUserEditDto.Email = values.Email;
+            appUserEditDto.City = values.City;
+            appUserEditDto.District = values.District;
+            appUserEditDto.ImageUrl = values.ImageUrl;
+            
+            return View(appUserEditDto);
 		}
 	}
 }
