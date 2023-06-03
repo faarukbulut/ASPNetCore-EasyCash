0fb32970dc24034d10daf206709238deb9c9dc02
faarukbulut <faarukbulut@gmail.com>
2023-06-03 07:05:35 +0300

    3- Customer Entitylerinin Yazılması

---
+++ EashCashIdentityProject/EntityLayer/Concrete/CustomerAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class CustomerAccount
+    {
+        public int CustomerAccountID { get; set; }
+
+        public string? CustomerAccountNumber { get; set; }
+
+        public string? CustomerAccountCurrency { get; set; }
+
+        public decimal CustomerAccountBalance { get; set; }
+
+        public string? BankBranch { get; set; }
+    }
+}
---
+++ EashCashIdentityProject/EntityLayer/Concrete/CustomerAccountProcess.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class CustomerAccountProcess
+    {
+        public int CustomerAccountProcessID { get; set; }
+
+        public string? ProcessType { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public DateTime ProcessDate { get; set; }
+    }
+}
