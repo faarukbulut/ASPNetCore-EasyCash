88d1ee80d8bf739e2b9d72029e5389f5e5d3afbf
faarukbulut <faarukbulut@gmail.com>
2023-06-06 10:45:33 +0300

    22- Login kullanıcının profil bilgilerini güncelleme

--- EashCashIdentityProject/DtoLayer/Dtos/AppUserDtos/AppUserEditDto.cs
+++ EashCashIdentityProject/DtoLayer/Dtos/AppUserDtos/AppUserEditDto.cs
@@ -23,5 +23,7 @@ namespace DtoLayer.Dtos.AppUserDtos
         public string PhoneNumber { get; set; }
 
         public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
     }
 }
--- EashCashIdentityProject/PresentationLayer/Controllers/MyProfileController.cs
+++ EashCashIdentityProject/PresentationLayer/Controllers/MyProfileController.cs
@@ -31,5 +31,32 @@ namespace PresentationLayer.Controllers
             
             return View(appUserEditDto);
 		}
+
+        [HttpPost]
+        public async Task<IActionResult> Index(AppUserEditDto appUserEditDto)
+        {
+            if (appUserEditDto.Password == appUserEditDto.ConfirmPassword)
+            {
+                var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+                user.PhoneNumber = appUserEditDto.PhoneNumber;
+                user.Surname = appUserEditDto.Surname;
+                user.City = appUserEditDto.City;
+                user.District = appUserEditDto.District;
+                user.Name = appUserEditDto.Name;
+                user.ImageUrl = "test";
+                user.Email = appUserEditDto.Email;
+                user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, appUserEditDto.Password);
+
+                var result = await _userManager.UpdateAsync(user);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Login");
+                }
+            }
+
+            return View();
+        }
 	}
 }
